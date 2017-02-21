f81ae16b9abd42c5936582d6ff42011419862a67
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2017-02-20 18:24:16 -0600

    Adds basic material UI dependencies to project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SiteRoutes from './components/SiteRoutes';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+const App = () => (
+  <MuiThemeProvider>
+    <SiteRoutes />
+  </MuiThemeProvider>
+);
 
 window.onload = () => {
-  ReactDOM.render(<SiteRoutes/>, document.getElementById('main'));
+  ReactDOM.render(<App />, document.getElementById('main'));
 };
